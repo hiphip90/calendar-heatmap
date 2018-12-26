409b2068231c2b1efc2bee40f73d0ea519a04dfc
Roman Vashurin <rvashurin@gmail.com>
2018-12-26 20:14:05 +0300

    Further lift tooltip

--- src/calendar-heatmap.js
+++ src/calendar-heatmap.js
@@ -187,7 +187,7 @@ function calendarHeatmap() {
             .html(tooltipHTMLForDate(d))
             .style('left', function () { return Math.floor(i / 7) * SQUARE_LENGTH + 'px'; })
             .style('bottom', function () {
-              return (6 - formatWeekday(d.getDay())) * (SQUARE_LENGTH + SQUARE_PADDING) + MONTH_LABEL_PADDING * 2 + SVG_PADDING + 'px';
+              return (6 - formatWeekday(d.getDay())) * (SQUARE_LENGTH + SQUARE_PADDING * 2) + MONTH_LABEL_PADDING * 2 + SVG_PADDING + 'px';
             });
         })
         .on('mouseout', function (d, i) {
