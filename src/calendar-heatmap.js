81514e73edf70fd0b67b019690cf4049945b70a0
Roman Vashurin <rvashurin@gmail.com>
2018-12-26 18:41:01 +0300

    Embarassing fix

--- src/calendar-heatmap.js
+++ src/calendar-heatmap.js
@@ -150,10 +150,10 @@ function calendarHeatmap() {
         .attr('width', function(d, i) {
           return 52 * (SQUARE_LENGTH + SQUARE_PADDING);
         })
-        .attr('class', function(d, i) {
+        .attr('class', 'calendar-heatmep')
+        .attr('height', function(d, i) {
           return 7 * (SQUARE_LENGTH + SQUARE_PADDING);
         })
-        .attr('height', height)
         .style('padding', '36px');
 
       dayRects = svg.selectAll('.day-cell')
